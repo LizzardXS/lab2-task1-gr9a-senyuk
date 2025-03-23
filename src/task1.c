b5111b9a96388f9334daf863213bb039f08016ee
LizzardXS <elizavetasenuk75@gmail.com>
2025-03-23 08:23:03 +0000

    first code

--- src/task1.c
+++ src/task1.c
@@ -1,8 +1,43 @@
 /* task1.c */
+#include <stdbool.h>
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+bool is_palindrome(int num) {
+  int original = num;
+  int reversed = 0;
+  int remainder;
+
+  while (num != 0) {
+    remainder = num % 10;
+    reversed = reversed * 10 + remainder;
+    num /= 10;
+  }
+
+  return original == reversed;
+}
+
+int main() {
+  int n;
+
+  printf("Введите размер массива: ");
+  scanf("%d", &n);
+
+  int arr[n];
+
+  printf("Введите %d натуральных чисел: ", n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  int count = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (is_palindrome(arr[i])) {
+      count++;
+    }
+  }
+
+  printf("Количество палиндромов в массиве: %d\n", count);
+
+  return 0;
 }
